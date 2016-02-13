fc05673f8b48a57c9448fd142aa08d645da2af76
arpitmandliya <mandliya@gmail.com>
2016-02-14 00:40:41 +0530

    Change-Id: I67bfc74253573d4106e79abaf6461f8059742f5e Signed-off-by: arpitmandliya <mandliya@gmail.com>

---
+++ SpringAutoWiringByTypeExample/src/main/java/org/arpit/java2blog/Capital.java
@@ -0,0 +1,15 @@
+
+package org.arpit.java2blog;
+
+public class Capital {
+
+    String capitalName;
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+    public void setCapitalName(String capitalName) {
+        this.capitalName = capitalName;
+    }
+}
